9394caab23e7dbdd9321e925f073fbf1b99e2b21
Ebrahim Ashraf <ebrahimashraf1996@gmail.com>
2024-05-27 09:30:28 +0300

    Snap Pixel

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -77,6 +77,25 @@
                    src="https://www.facebook.com/tr?id=4050682475145440&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Meta Pixel Code -->
+
+
+    <!-- Snap Pixel Code -->
+    <script type='text/javascript'>
+        (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+        {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+            a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+            r.src=n;var u=t.getElementsByTagName(s)[0];
+            u.parentNode.insertBefore(r,u);})(window,document,
+            'https://sc-static.net/scevent.min.js');
+
+        snaptr('init', '6d785bdc-d1f0-4610-b608-ba27f1c19c16', {
+            'user_email': '_INSERT_USER_EMAIL_'
+        });
+
+        snaptr('track', 'PAGE_VIEW');
+
+    </script>
+    <!-- End Snap Pixel Code -->
 </head>
 
 <body>
